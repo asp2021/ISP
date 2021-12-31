c379b249627128673ae8c9c57fe88acfaf9558c9
Daniel Perelman <danielfp@hotmail.com>
2021-12-31 10:39:24 -0300

    Agregar archivos de proyecto.

---
+++ ISP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ISP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ISP - Interface Segregation Principle" + "\n");
+
+            Console.WriteLine("No debemos tener intefaces muy grandes de tal forma que no obliguemos a los objetos que la implementan a implementar metodos que no necesiten." + "\n");
+
+            Console.WriteLine("SimplePrinter");
+            SimplePrinter simple = new SimplePrinter();
+            simple.Print("imprimo");
+            simple.Scan("escaneo");
+
+            Console.WriteLine();
+
+            Console.WriteLine("HPLaserJetPrinter");
+            HPLaserJetPrinter hplaser = new HPLaserJetPrinter();
+            hplaser.Print("imprimo");
+            hplaser.Print("escaneo");
+            hplaser.Fax("tambien tengo fax");
+            
+        }
+    }
+}
---
+++ ISP/SimplePrinter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    public class SimplePrinter: IPrint, IScan
+    {
+        public void Print(string content)
+        {
+            Console.WriteLine(content);
+        }
+
+        public void Scan(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+}
